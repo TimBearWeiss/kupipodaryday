8836a0793f7eae85632f6267c27375365f2f5ad3
Dmitrii Zverev <zverevtimbear@yandex.ru>
2024-07-07 15:42:53 +0300

    add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { WishesModule } from './wishes/wishes.module';
 import { WishlistsModule } from './wishlists/wishlists.module';
 import { OffersModule } from './offers/offers.module';
+import configuration from './config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfig } from './config/database-config.factory';
 
 @Module({
-  imports: [UsersModule, WishesModule, WishlistsModule, OffersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    TypeOrmModule.forRootAsync({ useClass: DatabaseConfig }),
+    UsersModule,
+    WishesModule,
+    WishlistsModule,
+    OffersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
